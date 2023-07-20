7d3ac965e229e39020b3b3b1db5b2a4bdfc3e50b
Emanuel Padilla <emanuehl159@gmail.com>
2023-07-19 22:52:23 -0500

    Added responsive styles to Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,18 @@ import { FaBars } from "react-icons/fa";
 import DarkModeButton from "./DarkModeButton";
 export default function Header({ setShowMobileMenu, showMobileMenu }) {
   return (
-    <header className="fixed z-10 min-w-full bg-white border-zinc-400 border-b-[1px] py-4 text-lg">
+    <header className="fixed z-10 min-w-full bg-white border-zinc-400 border-b-[1px] py-4 text-lg sm:text-sm sm:px-4 sm:flex sm:items-center sm:justify-between">
       <p className="text-center font-bold md:text-start">
         Emanuel Padilla
       </p>
       <FaBars className="absolute top-4 left-4 w-7 h-auto text-black dark:text-inherit" onClick={() => setShowMobileMenu(!showMobileMenu)}/>
-      <ul className="hidden text-sm">
+      <ul className="hidden sm:flex items-center sm:space-x-4">
         <li>Home</li>
         <li>Projects</li>
         <li>Certifications</li>
-        <DarkModeButton />
         <li>Contact Me</li>
         <li>Download CV</li>
+        <DarkModeButton />
       </ul>
     </header>
   );
