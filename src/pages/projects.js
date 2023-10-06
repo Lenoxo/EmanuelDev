f72a0666dbcc880619a0ae0ad85a04d145419336
Lenoxo <119215089+Lenoxo@users.noreply.github.com>
2023-10-05 22:53:46 -0500

    Fixed responsive problems with projectCards.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,7 +7,7 @@ export default function ProjectsPage() {
   return (
     <div className="min-h-screen flex flex-col pt-16 items-center">
       <h2 className="text-lg font-bold mb-8 mt-2">My Projects</h2>
-      <section className="w-auto h-auto grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center overflow-y-auto">
+      <section className="w-auto h-auto grid gap-2 grid-cols-1 sm:grid-cols-2 min-[860px]:grid-cols-3 min-[1100px]:grid-cols-4 place-items-center overflow-y-auto">
         {projects?.map((project, index) => (
           <ProjectCard key={index} projectData={project} />
         ))}
