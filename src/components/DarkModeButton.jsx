81d7e18c0c4b34cb146e57b3f287b9175382f0c5
Emanuel Padilla <emanuehl159@gmail.com>
2024-04-05 00:28:16 -0500

    Add Dark Mode status saving to localStorage, remove js-cookie

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 import { RiMoonClearLine } from "react-icons/ri";
 
 export default function DarkModeButton() {
+  const [DarkMode, setDarkMode] = useState(null);
+  useEffect(() => {
+    const isDarkModeActive = Boolean(localStorage.getItem("isDarkModeActive"));
+    setDarkMode(isDarkModeActive);
+  }, []); // This effect is used here to prevent this error: https://developer.school/snippets/react/localstorage-is-not-defined-nextjs
+
   const handleDarkModeToggle = () => {
     document.documentElement.classList.toggle("dark");
+    setDarkMode(!DarkMode); // For toggling it
+    localStorage.setItem("isDarkModeActive", DarkMode.toString());
   };
 
   return (
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import MainLayout from "@/components/MainLayout";
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const isDarkModeActive = Boolean(localStorage.getItem("isDarkModeActive"));
+    if (isDarkModeActive) {
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
   return (
     <>
       <MainLayout>
