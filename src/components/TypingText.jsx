fbb1e891dad494f881d22d16f69ca186cdb634a6
Emanuel Padilla <emanuehl159@gmail.com>
2024-04-20 20:32:28 -0500

    Refactor useTypewriter to prevent excesive rerenders.

---
+++ src/components/TypingText.jsx
@@ -0,0 +1,29 @@
+import { phrasesArray } from "@/data/homePhrases";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
+export function TypingText() {
+  const [writingText] = useTypewriter({
+    words: phrasesArray,
+    loop: 0,
+    typeSpeed: 130,
+  });
+  return (
+    <>
+      <div
+        id="writingText-container"
+        className="block sm:absolute lg:right-[-45%] md:top-[40%] sm:right-[-40%] sm:top-[15%]"
+      >
+        <h2 className="text-inherit sm:text-zinc-100 text-xl text-center sm:text-start sm:text-4xl font-semibold sm:w-[7.5rem] py-4">
+          I code
+          <span className="text-green-400 dark:text-sky-400 font-bold text-xl sm:text-4xl">
+            {" "}
+            {writingText}
+          </span>
+          <span className="font-bold text-zinc-300">
+            <Cursor />
+          </span>
+        </h2>
+      </div>
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,39 +2,17 @@ import { Technologies } from "@/components/Technologies";
 import TopProjects from "@/components/TopProjects";
 import { BsGithub, BsLinkedin, BsTwitterX } from "react-icons/bs";
 import StyledLink from "@/components/StyledLink";
-import { useTypewriter, Cursor } from "react-simple-typewriter";
-import { phrasesArray } from "@/data/homePhrases";
+import { TypingText } from "@/components/TypingText";
 
 export default function Home() {
-  const [writingText] = useTypewriter({
-    words: phrasesArray,
-    loop: 0,
-    typeSpeed: 130,
-  });
-
   return (
     <>
       <div className="h-screen bg-gradient-to-tr from-zinc-600 to-zinc-800 dark:from-gray-900 dark:to-gray-600">
         <div className="bg-gradient-to-tr px-4 from-zinc-300 via-zinc-50 to-white dark:from-zinc-900 dark:via-zinc-800 dark:to-zinc-700 relative h-full w-full sm:w-3/5 lg:max-w-2xl flex flex-col justify-center items-center sm:items-start">
           <h1 className="font-extrabold text-2xl sm:text-4xl sm:mb-4">
-            Full-Stack Developer
+            Full-Stack Developercito
           </h1>
-          <div
-            id="writingText-container"
-            className="block sm:absolute lg:right-[-45%] md:top-[40%] sm:right-[-40%] sm:top-[15%]"
-          >
-            <h2 className="text-inherit sm:text-zinc-100 text-xl text-center sm:text-start sm:text-4xl font-semibold sm:w-[7.5rem] py-4">
-              I code
-              <span className="text-green-400 dark:text-sky-400 font-bold text-xl sm:text-4xl">
-                {" "}
-                {writingText}
-              </span>
-              <span className="font-bold text-zinc-300">
-                <Cursor />
-              </span>
-            </h2>
-          </div>
-          {/* Later on, change this for a short paragraph explaining what you do. */}
+          <TypingText />
           <h2 className="text-md w-5/6 text-center sm:text-start">
             Proficient in HTML, CSS, JavaScript, and React. I work with
             libraries like Vite.js and Next.js and have backend experience with
